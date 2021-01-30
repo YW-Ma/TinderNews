1a4a05c830505114b76e9e6b4930cc4e19546645
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-31 03:37:22 +0800

    use retrofit and gson to implemnet a news client

---
+++ app/src/main/java/com/study/tindernews/Creidentials.java
@@ -0,0 +1,5 @@
+package com.study.tindernews;
+
+public class Creidentials {
+    public static final String API_KEY = "c13283ed9f294df0bda9a93891bf2b5e";
+}
--- app/src/main/java/com/study/tindernews/MainActivity.java
+++ app/src/main/java/com/study/tindernews/MainActivity.java
@@ -6,9 +6,18 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.study.tindernews.model.NewsResponse;
+import com.study.tindernews.network.NewsApi;
+import com.study.tindernews.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
@@ -28,6 +37,26 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
         // 2. link action bar and controller：action bar会显示名字、负责返回
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class); // reflection
+        // one call is one task, enqueue the task to let executor do it.
+        // give a callback to handle response
+        api.getTopHeadlines("CN").enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                // if success print body, else print response(message, code)
+                if (response.isSuccessful()) {
+                    Log.d("getTopHeadlines", response.body().toString());
+                } else {
+                    Log.d("getTopHeadlines", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.d("getTopHeadlines", t.toString());
+            }
+        });
     }
 
     // 支持action bar的回退到home的操作
