fc93f2c0540a023b871b5e67d9693488419abc9b
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-31 02:44:36 +0800

    use jetpack navigation component

--- app/src/main/java/com/study/tindernews/MainActivity.java
+++ app/src/main/java/com/study/tindernews/MainActivity.java
@@ -18,14 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view); // nav bar --> the navigation graph
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment); // host fragment --> activity_main/host_fragment
+        BottomNavigationView navView = findViewById(R.id.nav_view); // 找到bottom
+        // 找到fragment host     host fragment --> activity_main/host_fragment
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        // 把fragment的controller取出来，它可以告诉我们当前显示的是谁、也可以控制跳转
         navController = navHostFragment.getNavController();
-        NavigationUI.setupWithNavController(navView, navController); // use navigation graph to setup the navController
+        // 1. link navView and controller
+        NavigationUI.setupWithNavController(navView, navController);
+        // 2. link action bar and controller：action bar会显示名字、负责返回
         NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
+    // 支持action bar的回退到home的操作
     @Override
     public boolean onSupportNavigateUp() {
         return navController.navigateUp();
